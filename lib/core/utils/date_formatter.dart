35b684811cb25b935cd8db3b200ecd6459f835b9
김지헌(KimJiHeon) <wlgjsrns@gmail.com>
2026-09-03 15:01:27 +0900

    chore: 현재 식단 기록을 조회해오는 provider 추가, 날짜 관련 유틸 함수 구현

--- lib/core/utils/date_formatter.dart
+++ lib/core/utils/date_formatter.dart
@@ -1,18 +1,27 @@
 class DateFormatter {
   DateFormatter._();
 
+  // yyyy.mm.dd
   static String toDateLabel(DateTime date) {
-    // TODO: Phase 4에서 구현
-    throw UnimplementedError("Phase 4에서 구현 예정");
+    return '${date.year}.'
+      '${date.month.toString().padLeft(2, '0')}.'
+      '${date.day.toString().padLeft(2, '0')}';
   }
 
+  // yyyy년 m월 d일 (일~토)
   static String toKoreanDate(DateTime date) {
-    // TODO: Phase 4에서 구현
-    throw UnimplementedError("Phase 4에서 구현 예정");
+    const weekdays = ['월', '화', '수', '목', '금', '토', '일'];
+    final wd = weekdays[date.weekday - 1];
+    return '${date.year}년 ${date.month}월 ${date.day}일 ($wd)';
   }
 
+  // 방금 전 / N분 전 / N시간 전 / N일 전
   static String toTimeLabel(DateTime date) {
-    // TODO: Phase 4에서 구현
-    throw UnimplementedError("Phase 4에서 구현 예정");
+    final diff = DateTime.now().difference(date);
+    if (diff.inMinutes < 1) return '방금 전';
+    if (diff.inHours < 1) return '${diff.inMinutes}분 전';
+    if (diff.inDays < 1) return '${diff.inHours}시간 전';
+    if (diff.inDays < 30) return '${diff.inDays}일 전';
+    return toDateLabel(date);
   }
 }
--- lib/providers/meal_record_provider.dart
+++ lib/providers/meal_record_provider.dart
@@ -1,8 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:honey/data/models/meal_record.dart';
 import 'package:honey/data/repositories/impl/supabase_meal_record_repository.dart';
 import 'package:honey/data/repositories/meal_record_repository.dart';
 import 'package:honey/main.dart';
 
 final mealRecordRepositoryProvider = Provider<MealRecordRepository>((ref) {
   return SupabaseMealRecordRepository(supabase);
+});
+
+final mealRecordProvider = FutureProvider.family<MealRecord, String>((ref, id) {
+  return ref.read(mealRecordRepositoryProvider).getRecord(id);
 });
